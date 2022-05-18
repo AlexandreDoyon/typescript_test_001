a68403665753517598327aec6d454ab592889329
Alexandre Doyon <philosophalex@gmail.com>
2022-05-18 04:54:59 -0400

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>TEST 001</h1>
+        <h1 className={styles.title}>TEST 1</h1>
 
         <p className={styles.description}>
           Get started by editing{" "}
